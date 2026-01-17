1bff9a988aa0a15050ae070d5d08715c287ee98a
CHoU <handsomecheung@gmail.com>
2026-01-17 19:33:37 +0900

    build: decouple encode and decode binaries to reduce dependencies

--- compile.sh
+++ compile.sh
@@ -3,7 +3,8 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-cargo build --release
+cargo build --bin cube-encode --release --no-default-features --features encode
+cargo build --bin cube-decode --release --features decode
 
 cargo build --target wasm32-unknown-unknown --release --no-default-features --features wasm
 
