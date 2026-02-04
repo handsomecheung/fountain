b819dba8a08a19185068b88991c18720802e2ef0
CHoU <handsomecheung@gmail.com>
2026-02-04 21:58:42 +0900

    change decode-camera-web port

--- script/run-decode-camera-web.sh
+++ script/run-decode-camera-web.sh
@@ -9,7 +9,7 @@ ${CUBE_DOCKER} stop ${CUBE_CONTAINER_CAMERA_WEB} 2>/dev/null || true
 ${CUBE_DOCKER} rm ${CUBE_CONTAINER_CAMERA_WEB} 2>/dev/null || true
 ${CUBE_DOCKER} run --rm -d \
     --name ${CUBE_CONTAINER_CAMERA_WEB} \
-    -p 37290:80 \
+    -p 37250:80 \
     -v "$(pwd)/script/nginx.decode-camera-web.conf:/etc/nginx/nginx.conf" \
     -v "$(pwd)/www:/cube/www" \
     nginx:latest
